9fd37d081c2509b8c147c1cdbd6c11f1d578aaee
Maria Arellano <mariflower93@Maria-Arellanos-MacBook-Pro.local>
2017-08-05 15:34:13 -0400

    index

--- indexJS.js
+++ indexJS.js
@@ -1,8 +1,31 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+app.config(function($routeProvider,$locationProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "index.htm"
+        templateUrl : "index.html"
     })
 
+    .when('/view1',{
+       templateUrl: 'view1.html',
+       controller: 'View1Ctrl',
+})
+    .when('view2',{
+       templateUrl: 'viev2.html',
+       controller: 'View2Ctrl',
+
+    })
+    .otherwise({ 
+    redirectTo: '/index.html'
 });
+
+$locationProvider.hashPrefix('');
+
+});
+
+
+
+
+
+
+
+
