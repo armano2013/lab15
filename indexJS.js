65836e4245cf5ca3f8fa6dc5e86404bb099a458c
Mohammed Ali <fh0420@wayne.edu>
2017-08-05 16:45:38 -0400

    Try to fix this

--- indexJS.js
+++ indexJS.js
@@ -1,31 +1,22 @@
-var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider,$locationProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "index.html"
-    })
-
-    .when('/view1',{
-       templateUrl: 'view1.html',
-       controller: 'View1Ctrl',
-})
-    .when('view2',{
-       templateUrl: 'view2.html',
-       controller: 'View2Ctrl',
-
-    })
-    .otherwise({ 
-    redirectTo: '/index.html'
-});
-
-$locationProvider.hashPrefix('');
-
-});
-
-
-
-
-
-
-
-
+// app.config(function($routeProvider,$locationProvider) {
+//     $routeProvider
+//     .when("/", {
+//         templateUrl : "index.html"
+//     })
+//
+//     .when('/view1',{
+//        templateUrl: 'view1.html',
+//        controller: 'View1Ctrl',
+// })
+//     .when('view2',{
+//        templateUrl: 'view2.html',
+//        controller: 'View2Ctrl',
+//
+//     })
+//     .otherwise({
+//     redirectTo: '/index.html'
+// });
+//
+// $locationProvider.hashPrefix('');
+//
+// });
