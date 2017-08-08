229b290c92b571609b5833e58eb9c7edf9c92778
Mohammed Ali <fh0420@wayne.edu>
2017-08-08 19:02:34 -0400

    added files without comments

--- View1/fizzBuzz.js
+++ View1/fizzBuzz.js
@@ -1,14 +1,14 @@
-// for (var i=1;i<101;i++){
-//
-//   if(i%15===0){//busca todos los numeros divisibles entre 15
-//     console.log('FizzBuzz');//y los reemplaza con FizzBuzz
-//
-//   }else if(i%3===0){// busca todos los numeros divisibles entre 3
-//     console.log ('Fizz');//y los reemplaza con Fizz
-//
-//   }else if (i%5===0){// busca todos los numeros divisibles entre 5
-//     console.log('Buzz');//y los reemplaza con Buzz
-//
-//     }else{
-//       console.log(i);
-//   }
+for (var i=1;i<101;i++){
+
+  if(i%15===0){//busca todos los numeros divisibles entre 15
+    console.log('FizzBuzz');//y los reemplaza con FizzBuzz
+
+  }else if(i%3===0){// busca todos los numeros divisibles entre 3
+    console.log ('Fizz');//y los reemplaza con Fizz
+
+  }else if (i%5===0){// busca todos los numeros divisibles entre 5
+    console.log('Buzz');//y los reemplaza con Buzz
+
+    }else{
+      console.log(i);
+  }
