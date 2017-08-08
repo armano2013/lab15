45840398710e7bd70091b40b70667fa25e36d77d
Maria Arellano <mariflower93@Maria-Arellanos-MacBook-Pro.local>
2017-08-08 19:21:36 -0400

    pictures

--- main/main-controller.js
+++ main/main-controller.js
@@ -1,4 +1,10 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
+        $scope.picture = {
+        waterfall : 'pictureView1.jpg',
+        waterpic: 'pictureView2.jpg',
+       
+    };
+
    
-})
+});
