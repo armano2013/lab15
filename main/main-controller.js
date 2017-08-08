a0e4b6ed985c7ab4df2294df81c3b64b6f414f58
Maria Arellano <mariflower93@Maria-Arellanos-MacBook-Pro.local>
2017-08-08 19:32:55 -0400

    change

--- main/main-controller.js
+++ main/main-controller.js
@@ -1,6 +1,31 @@
 var app = angular.module('myApp', []);
+
+
+app.config(function($routeProvider,$locationProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "index.html"
+    })
+
+    .when('/view1',{
+       templateUrl: 'view1.html',
+       controller: 'view1.html',
+})
+    .when('view2',{
+       templateUrl: 'view2.html',
+       controller: 'view2.html',
+
+    })
+    .otherwise({
+    redirectTo: '/index.html'
+});
+
+$locationProvider.hashPrefix('');
+
+});
+
 app.controller('myCtrl', function($scope) {
-        $scope.picture = {
+        $scope.pictures = {
         waterfall : 'pictureView1.jpg',
         waterpic: 'pictureView2.jpg',
        
