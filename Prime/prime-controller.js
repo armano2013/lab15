059059071bdd4b9659946e40ccf4fb2b03046741
Mohammed Ali <fh0420@wayne.edu>
2017-08-09 19:42:07 -0400

    added the prime to the code

--- Prime/prime-controller.js
+++ Prime/prime-controller.js
@@ -1,49 +1,25 @@
 var app = angular.module("myApp");
 app.controller('PrimeController', function($scope) {
-    var primeList = [];
-    // fill with true
-    for(var i = 0; i <= 1000; i++){
-        primeList[i] = {
-            id: i,
-            flag: true};
-    }
-    // set false
-    for(var i = 0; i <= 1000; i++){
-        for(var j = 2; j < i; j++){
-            if(i%j === 0){
-                primeList[i].flag = false;
-                break;
-            }
-        }
-    }
-
-    $scope.primeFlags = function(){
-      return primeList.slice($scope.fromVal, $scope.toVal);
-
-    };
-});
-
-
-
-function test_prime(n)
-{
-
-  if (n===1)
-  {
-    return false;
-  }
-  else if(n === 2)
-  {
-    return true;
-  }else
-  {
-    for(var x = 2; x<n; x++)
-    {
-      if(n % x === 0)
+    $scope.primeTest = function (number){
+      if (number===1)
       {
-        return false;
+        return number + ' is Not Prime';
       }
+      else if(number === 2)
+      {
+        return `${number} is Prime`;
+      }else
+      {
+        for(var x = 2; x<number; x++)
+        {
+          if(number % x === 0)
+          {
+            return number + ' is Not Prime';
+          }
+        }
+        return `${number} is Prime`;
+      }
+
     }
-    return true;
-  }
-}
+
+});
